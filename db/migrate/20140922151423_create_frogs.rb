b84c295d58709f0610c28f93dd6117b2081ff57c
Lisa Lee <snickers495@gmail.com>
2017-12-07 20:35:34 +0000

    automatically backed up by learn

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,9 +1,9 @@
 class CreateFrogs < ActiveRecord::Migration
   # code goes here
-  def change 
+  def change
     create_table :frogs do |f|
-        f.string :name 
-        f.string :color 
+        f.string :name
+        f.string :color
       end 
-  end 
+  end
 end
