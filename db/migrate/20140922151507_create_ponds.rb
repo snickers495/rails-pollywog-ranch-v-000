64b1e5cd8960b6bf074de258a677935df9aa8b64
Lisa Lee <snickers495@gmail.com>
2017-12-07 20:36:16 +0000

    automatically backed up by learn

--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,9 +1,9 @@
 class CreatePonds < ActiveRecord::Migration
   # code goes here
-  def change 
+  def change
     create_table :ponds do |p|
-      p.string :name 
-      p.string :water_type 
+      p.string :name
+      p.string :water_type
     end
-  end 
+  end
 end
